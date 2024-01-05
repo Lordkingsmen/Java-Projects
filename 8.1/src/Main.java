a9b2a6cb7b760e79b5b83eea2c0f50be74a74e9d
CHRISTIAN AVELINO-MILLER <AVELICHR000@student.evergreenps.org>
2024-01-05 10:36:11 -0800

    4
    4000

--- 8.1/src/Main.java
+++ 8.1/src/Main.java
@@ -22,4 +22,16 @@ public class Main
             System.out.println();
         }
 	}
+	
+	public int dateFashion(int you, int date)
+	{
+		if(you>=8||date>=8)
+		{
+			if(you<2&&date<2)
+				return 2;
+		}
+		else if(you<2&&date<2)
+			return 1;
+		return 0;
+	}
 }
